39c779c4e87d48771f2eab7bb8a3c46ffbb84b79
kyung hyuk cho <92010711+kyunghyukCHO@users.noreply.github.com>
2023-06-13 14:17:12 +0900

    Delete src directory

--- src/Server/Main.java
+++
@@ -1,9 +0,0 @@
-package Server;
-
-public class Main {
-    public static void main(String[] args) {
-        ServerController serverController = new ServerController();
-        serverController.setPort(8888);
-        serverController.start();
-    }
-}
